a6b1478be0f6d5e6c4ada32f373dd900debf4af4
Volodymyr Antoniuk <voxtobox@gmail.com>
2025-03-18 21:22:00 -0700

    init

---
+++ main.py
@@ -0,0 +1,95 @@
+import mimetypes
+import pathlib
+from datetime import datetime
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+import json
+from jinja2 import Environment, FileSystemLoader
+
+storage_file = "storage/data.json"
+env = Environment(loader=FileSystemLoader("."))
+
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers["Content-Length"]))
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        data_dict = {
+            key: value for key, value in [el.split("=") for el in data_parse.split("&")]
+        }
+        self.write_form_data(data_dict)
+        self.send_response(302)
+        self.send_header("Location", "/")
+        self.end_headers()
+
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == "/":
+            self.send_html_file("index.html")
+        elif pr_url.path == "/message":
+            self.send_html_file("message.html")
+        elif pr_url.path == "/read":
+            self.send_read_page()
+        else:
+            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                self.send_static()
+            else:
+                self.send_html_file("error.html", 404)
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        with open(filename, "rb") as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        with open(f".{self.path}", "rb") as file:
+            self.wfile.write(file.read())
+
+    def write_form_data(self, formData):
+        timestamp = datetime.now().isoformat()
+        try:
+            with open(storage_file, "r", encoding="utf-8") as file:
+                data = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = {}
+        data[timestamp] = formData
+
+        with open(storage_file, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+    def send_read_page(self):
+        try:
+            with open(storage_file, "r", encoding="utf-8") as file:
+                messages = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            messages = {}
+
+        template = env.get_template("read.html")
+        html_content = template.render(data=messages)
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(html_content.encode("utf-8"))
+
+
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ("", 3000)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        http.server_close()
+
+
+if __name__ == "__main__":
+    run()
